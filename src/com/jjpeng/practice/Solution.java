a9ebb77a8ab9526f408945727c37bbf8f1759412
JJPeng <pengjiajun755@163.com>
2022-07-13 18:20:58 +0800

    题目实践：交易员都在哪些不同的城市工作过

--- src/com/jjpeng/practice/Solution.java
+++ src/com/jjpeng/practice/Solution.java
@@ -6,6 +6,7 @@ import com.jjpeng.practice.domain.Transaction;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Solution {
@@ -32,8 +33,20 @@ public class Solution {
                 .sorted(Comparator.comparing(Transaction::getValue))
                 .collect(Collectors.toList());
         System.out.println("找出2011年发生的所有交易，并按交易额排序（从低到高）:");
-        System.out.print(result1);
-        //todo: 交易员都在哪些不同的城市工作过？
+        System.out.println(result1);
+
+        //交易员都在哪些不同的城市工作过？
+        List<String> result2 = transactions.stream()
+                .map(t -> t.getTrader().getCity())
+                .distinct()
+                .collect(Collectors.toList());
+        Set<String> result3 = transactions.stream()
+                .map(t -> t.getTrader().getCity())
+                .collect(Collectors.toSet());
+        System.out.println("交易员都在哪些不同的城市工作过:");
+        System.out.println(result2);
+        System.out.println(result3);
+
         //todo: 查找所有来自于剑桥的交易员，并按姓名排序
         //todo: 返回所有交易员的姓名字符串，按字母顺序排序
         //todo: 有没有交易员是在米兰工作的？
