e095a7d69c46d0651b06b29b8016ce81415ae873
JJPeng <pengjiajun755@163.com>
2022-07-15 12:29:46 +0800

    数值流：将数值流重新装箱为对象流（一般流）

--- src/com/jjpeng/stream/NumericStreamsDemo.java
+++ src/com/jjpeng/stream/NumericStreamsDemo.java
@@ -2,6 +2,7 @@ package com.jjpeng.stream;
 
 import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class NumericStreamsDemo {
 
@@ -12,5 +13,10 @@ public class NumericStreamsDemo {
                 .mapToInt(Dish::getCalories);
         int sum = intStream.sum();
         System.out.println(sum);
+
+        //将数值流重新装箱为对象流（一般流）
+        IntStream unboxedStream = menu.stream()
+                .mapToInt(Dish::getCalories);
+        Stream<Integer> boxed = unboxedStream.boxed();
     }
 }
